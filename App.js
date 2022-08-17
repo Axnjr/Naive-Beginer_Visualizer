453219cdc39d6eaa3adba587d2c548bfc4355cb9
Axnjr <111436589+Axnjr@users.noreply.github.com>
2022-08-17 18:16:49 +0530

    Update App.js

--- App.js
+++ App.js
@@ -15,7 +15,6 @@ export default function App(){
             </div>
             <Routes>
                 <Route path="/searching" element={<Searching/>}/>
-                <Route path="/reversing" element={<Reverse/>}/>
                 <Route path="/sorting" element={<KarleParent/>}/>
             </Routes>
         </Router>
