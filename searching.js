0818dba4343b42b7de328b54c99d2c6a185795d7
Yakshit Chhipa <111436589+Axnjr@users.noreply.github.com>
2022-08-18 18:18:26 +0530

    Update searching.js

--- searching.js
+++ searching.js
@@ -14,6 +14,7 @@ class SearchingParent extends React.Component{
       this.state={user:prompt("Enter Array Length : Min 2 | Max 200 ")}
     }}
 export class Searching extends SearchingParent{
+    
     constructor(){
         super();
         for(let i=0;i<this.state.user;i++){a[i] = Math.floor(Math.random()*100)}
@@ -22,21 +23,27 @@ export class Searching extends SearchingParent{
         window.aval = a
         console.log(a)
     }
+    
     linary_search = () => {
         let ans = null;
-        let lid = setInterval(()=>{
-            for(let i=0;i<this.state.clicks;i++){
+        let lid = setInterval(()=>
+          {
+            for(let i=0;i<this.state.clicks;i++)
+            {
                 document.querySelector('.arr'+i).style.backgroundColor = '#333';
-                if(window.aval[i]==this.state.val){
+                if(window.aval[i]===this.state.val){
                     let ans=i;
                     document.querySelector('.arr'+ans).style.backgroundColor = '#fbbf24';
-                }}
+                }
+           }
             this.setState({clicks:this.state.clicks+1})
-            if(this.state.clicks==this.state.userlen){
+            if(this.state.clicks===this.state.userlen)
+            {
                 clearInterval(lid);  
                 console.log("end");
                 this.setState({result:ans});
-            }},this.state.speed)
+            }
+          },this.state.speed)
         }
     binary_search = () => {
         let sa = window.aval.sort(function(a, b){return a - b})
@@ -44,26 +51,32 @@ export class Searching extends SearchingParent{
         let looking_for = this.state.val
         let s = 0
         let l = this.state.userlen-1
+        
         function ser(){
             while (s <= l ) {
                 let mid = Math.floor((s + l) / 2);
-                if(sa[mid]==looking_for) {
+                if(sa[mid]===looking_for) {
                     return mid;}
                 else if(sa[mid] < looking_for) {
                     s = mid + 1;}
                 else {l = mid - 1;}}}
+        
         let plus = ser()+1
         let minus = ser()-1
-        if(ser()==undefined){
+        if(ser()===undefined)
+        {
             document.querySelector('.arrcon').style.display = 'none';
             document.getElementById("if").style.display = 'block';
         }
-        if(ser()!=undefined){
-            if(sa[ser()]===sa[plus]){
+        if(ser()!==undefined)
+        {
+            if(sa[ser()]===sa[plus])
+            {
                 document.querySelector('.arr'+ser()).style.backgroundColor = '#fbbf24';
                 document.querySelector('.arr'+plus).style.backgroundColor = '#fbbf24';
             }
-            else if(sa[ser()]===sa[minus]){
+            else if(sa[ser()]===sa[minus])
+            {
                 document.querySelector('.arr'+ser()).style.backgroundColor = '#fbbf24';
                 document.querySelector('.arr'+minus).style.backgroundColor = '#fbbf24';
             }
@@ -72,18 +85,22 @@ export class Searching extends SearchingParent{
             }
         }
     }
-    render(){
-        if(this.state.userlen>30){
+    
+    render()
+    {
+        if(this.state.userlen>30)
+        {
             style_value = 'wrap';
             margin_ratio = 100;
             size_ratio = 1000;
             font_ratio = 35;
         }        else{style_value = 'no-wrap';margin_ratio=10;size_ratio=300;font_ratio=10;}
+        
         return(
             <>
                 <div className='arrcon' style={{flexWrap:style_value,marginTop:'8rem'}}>{ 
                   this.state.count.split(',').map((val,idx) => {
-                    if(val==''){val=Math.floor(Math.random()*100)}
+                    if(val===''){val=Math.floor(Math.random()*100)}
                     return(
                         <>
                             <div id={val} style={{height:size_ratio/this.state.userlen+'mm',width:size_ratio/this.state.userlen+'mm'
